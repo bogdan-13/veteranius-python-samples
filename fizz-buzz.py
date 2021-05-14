1aeebc1e5a3c877cd930fc41b274d54fc7288966
Bogdan Kalnybolotchuk <bogdan.kalnybolotchuk@gmail.com>
2021-05-15 01:26:25 +0300

    add "Fizz Buzz"

---
+++ fizz-buzz.py
@@ -0,0 +1,9 @@
+for count in range(1, 101):
+    if count % 3 == 0 and count % 5 == 0:
+        print('Fizz-Buzz')
+    elif count % 3 == 0:
+        print('Fizz')
+    elif count % 5 == 0:
+        print('Buzz')
+    else:
+        print(count)
