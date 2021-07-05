9ef44d4ce1ad5e65b4dbb34cb968ae55162f65b8
Vsevolod Chernetskyi <vchernetskyi993@gmail.com>
2021-07-05 14:32:19 +0300

    lecture-7 - prettified examples from lecture

--- lecture-7/exceptions.py
+++ lecture-7/exceptions.py
@@ -1,16 +1,37 @@
-class ParentError(Exception):
-    pass
+# you could throw any exception with raise keyword
+raise ValueError('Hello my first error!')
 
-class ChildError(ParentError):
-    pass
-
-class AnotherChildError(ParentError):
-    pass
-
-for cls in [ParentError, ChildError, AnotherChildError]:
+def divide(a: int, b: int) -> float:
+    result = 0.0
     try:
-        raise ParentError()
-    except ParentError:
+        result = a / b
+    except ZeroDivisionError as err: # you can catch exceptions from try block
+        print("dividing by zero...")
+        raise # you can re-raise caught exception
+        raise ValueError from err # you can throw new exception that reuses caught one
+    else: # else block executes only if no exception was thrown
+        print("not dividing by zero...")  
+        return result
+    finally: # finally blocks executes in either case
+        print("finally...")
+
+# to create your custom exceptions
+# create module-specific Error class
+class Error(Exception):
+    pass
+
+# inherit your Error class through your custom exception(s)
+class ChildError(Error):
+    pass
+
+class AnotherChildError(Error):
+    pass
+
+for cls in [Error, ChildError, AnotherChildError]:
+    try:
+        raise cls()
+    except Error: # in such scenario all 3 exceptions will be caught in the first block
+                  # because all 3 of them are instances on your custom Error class
         print("MyError")
     except ChildError:
         print("D")
@@ -19,21 +40,3 @@ for cls in [ParentError, ChildError, AnotherChildError]:
     
 
 
-def divide(a: int, b: int) -> float:
-    result = 0.0
-    try:
-        result = a / b
-    except ZeroDivisionError:
-        print("dividing by zero...")
-        return 0.0
-    else:
-        print("not dividing by zero...")
-        return result
-    finally:
-        print("finally...")
-
-def divide_if(a: int, b: int) -> float:
-    if b == 0:
-        return 0.0
-    else:
-        return a / b
--- lecture-7/io.py
+++ lecture-7/io.py
@@ -1,27 +1,56 @@
-with open('temp.file', 'r+') as temp:
-    temp.write('Hello')
+file_path = 'temp.file'
+name = 'John'
 
-# try:
-#     temp = open('temp.file', 'x')
-#     temp.write('Hello Python World')
-# finally:
-#     temp.close()
+# modern Python's string interpolation, use it for new code
+# supports a lot of formatting options for details see https://docs.python.org/3/library/string.html#formatspec
+print(f'Hello, {name}')
 
+# string format
+print('Hello, {}'.format(name))
+# modulo string formatting
+print('Hello, %s' % name)
 
+# write string values to file
+# different modes are listed in https://docs.python.org/3/library/functions.html#open
+with open(file_path, 'w') as temp:
+    temp.writelines(['Hello Python World!', 'What do we want?', 'Multi-line lambda!'])
+
+# read file as string
+with open(file_path, 'r') as temp:
+    print(f'{temp.read()=}')
+
+# to work with big files you should not load all file to memory, but read it partially
+with open(file_path, 'r') as temp:
+    for line in temp:
+        print(f'{line=}')
+
+# old-school way of working with files (without with keyword)
+try:
+    temp = open(file_path, 'r')
+    print(f'{temp.read()=}')
+finally:
+    temp.close()
+
+# if your class should open and reuse opened resources this is how to make it work with with
 class MyCloseable:
     def __init__(self):
-        self.resource = open('temp.file', 'r+')
+        self.resource = open(file_path, 'r+')
 
     def write_str(self, value: str = 'Hello'):
         self.resource.write(value)
 
     def __enter__(self):
+        print('Opening MyCloseable...')
         return self
 
-    def __exit__(self,exc_type, exc_value, traceback):
+    def __exit__(self, exc_type, exc_value, traceback):
         print('Closing MyCloseable...')
         self.resource.close()
 
 with MyCloseable() as temp:
-    temp.write_str('My Python created file')
+    value = 'My Python created file'
+    print(f'Writing "{value}" to resource')
     temp.write_str()
+
+import os
+os.remove(file_path)
